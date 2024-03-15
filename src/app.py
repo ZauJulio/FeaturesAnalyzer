75f64dc996783b416e95fcc080aa9e720f37685e
ZauJulio <zaujulio.dev@gmail.com>
2024-03-14 21:00:49 -0300

    init: start project settings

---
+++ src/app.py
@@ -0,0 +1,51 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk, Gio
+
+
+class FeaturesAnalyzer(Gtk.Application):
+    def __init__(self):
+        super().__init__(
+            application_id="zaujulio.research.FeaturesAnalyzer",
+            flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
+        )
+
+        self.create_actions("quit", lambda *_: self.quit(), ["<primary>q"])
+        self.create_actions("about", lambda *_: self.on_about_action(), ["<primary>a"])
+
+    def create_actions(self, name, callback, accel):
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+
+        self.add_action(action)
+        self.set_accels_for_action(f"app.{name}", accel)
+
+    def on_about_action(self, widget, _):
+        """Callback for the app.about action."""
+        about = Gtk.AboutDialog(
+            transient_for=self.props.active_window,
+            modal=True,
+            program_name="FeaturesAnalyzer",
+            logo_icon_name="zaujulio.research.FeaturesAnalyzer",
+            version="0.1.0",
+            authors=["Zaú Júlio"],
+            copyright="© 2024 Zaú Júlio",
+        )
+
+        about.present()
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        builder = Gtk.Builder()
+        builder.add_from_file("ui/window.ui")
+        builder.connect_signals(self)
+
+        self.window = builder.get_object("MainWindow")
+        self.add_window(self.window)
+        self.window.show_all()
