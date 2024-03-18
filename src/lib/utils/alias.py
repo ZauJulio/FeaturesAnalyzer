d361d0d5e02cf85f8d51264948e23616f495540d
ZauJulio <zaujulio.dev@gmail.com>
2024-03-18 11:35:28 -0300

    fix: correction of os path separator

--- src/lib/utils/alias.py
+++ src/lib/utils/alias.py
@@ -7,13 +7,20 @@ def at(path: str):
     Args:
         path (str): The path to the file.
         Must start with: ["@layout", "@icon"]
+        and followed by the path to the file using "/" as separator.
 
     Returns:
         str: The absolute path of the file.
     """
     seg = path.split("/")
 
+    if len(seg) < 2:
+        raise ValueError("Invalid path.")
+
+    tag = seg[0]
+    paths = os.sep.join(seg[1:])
+
     return {
-        "@layout": os.path.join(os.getcwd(), "ui", "layouts", f"{seg[1]}.ui"),
-        "@icon": os.path.join(os.getcwd(), "..", "assets", "icons", seg[1]),
-    }[seg[0]]
+        "@icon": os.path.join(os.getcwd(), "..", "assets", "icons", paths),
+        "@layout": os.path.join(os.getcwd(), "ui", "layouts", f"{paths}.ui"),
+    }[tag]
